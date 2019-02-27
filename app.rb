7f8fbb92366e91dd3a0162a89bfae63e691564e1
Trent Moss <trentmoss05@gmail.com>
2019-02-27 16:24:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
     answer
   end
 
-  get 'say/:word1/:word2/:word3/:word4/:word5' do
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 end
