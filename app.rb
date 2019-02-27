2d8f3b337debf83593b58815bfb55a0138ddf9a7
Trent Moss <trentmoss05@gmail.com>
2019-02-27 16:28:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,4 +24,20 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
+
+  get '/:operation/:number1/:number2' do
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+    answer = "Cannot perform that action"
+    case params[:operation]
+    when 'add'
+      answer = (num1 + num2).to_s
+    when 'subtract'
+      answer = (num1 - num2).to_s
+    when 'multiply'
+      answer = (num1 * num2).to_s
+    when 'divide'
+      answer = (num1 / num2).to_s
+    end
+  end
 end
